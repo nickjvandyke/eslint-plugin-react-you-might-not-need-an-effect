e65513df307ab26525f30fb026ceaa5870f6af32
Nick van Dyke <nickjvandyke15@gmail.com>
2026-02-15 10:28:22 -0700

    fix(types): type the recommended strict configs
    https://github.com/nickjvandyke/eslint-plugin-react-you-might-not-need-an-effect/issues/51

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,7 +3,9 @@ import type { ESLint, Linter } from "eslint";
 declare const plugin: ESLint.Plugin & {
   configs: {
     recommended: Linter.Config;
+    strict: Linter.Config;
     "legacy-recommended": Linter.LegacyConfig;
+    "legacy-strict": Linter.LegacyConfig;
   };
 };
 export default plugin;
